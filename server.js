ace99462af20bc43231aad5aae1e88ffd70f1ee5
Divyyanshu <divvyyanshuutailor@gmail.com>
2024-08-04 04:55:32 +0530

    first server setup

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express")
+
+// rest objeet
+const app = express();
+
+// listen server
+app.listen(8080 , ()=>{
+    console.log("server Running in dev mode on port johe 8080")
+})
